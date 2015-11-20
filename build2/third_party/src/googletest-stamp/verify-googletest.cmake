9b10e063cc9e70d6838bc8984aa0a2bfc68a2800
Tom Nickson <tom.nickson@gmail.com>
2015-11-20 16:15:12 +0000

    type specifications wrong on kronvecstack

---
+++ build2/third_party/src/googletest-stamp/verify-googletest.cmake
@@ -0,0 +1,4 @@
+set(file "/Users/tom/Code/KronMat/build2/third_party/src/gtest-1.7.0.zip")
+message(STATUS "verifying file...
+     file='${file}'")
+message(STATUS "verifying file... warning: did not verify file - no URL_HASH specified?")
